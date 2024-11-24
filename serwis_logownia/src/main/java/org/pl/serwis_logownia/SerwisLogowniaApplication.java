bf9388c0bfd933efe4874d4f67ca2dba196583ef
Kacper <kacper.klimas@poczta.fm>
2024-11-24 21:50:45 +0100

    Inicjalizacja trzech mikrosulug

---
+++ serwis_logownia/src/main/java/org/pl/serwis_logownia/SerwisLogowniaApplication.java
@@ -0,0 +1,13 @@
+package org.pl.serwis_logownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisLogowniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisLogowniaApplication.class, args);
+    }
+
+}
