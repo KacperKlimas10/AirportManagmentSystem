4d1016a739191990d1d8e0dd8f33bfc938347c6f
Kacper <kacper.klimas@poczta.fm>
2024-11-24 21:25:20 +0100

    Inicjacja projektu

---
+++ src/test/java/org/pl/lotnisko_projekt/LotniskoProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pl.lotnisko_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotniskoProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
