6d720a8c23a08c6e0319cb0ca19105ef1b5e44f5
Kacper <kacper.klimas@poczta.fm>
2024-11-26 22:26:19 +0100

    First Rest Controller

--- serwis_logownia/src/main/java/org/pl/serwis_logownia/AuthService.java
+++ serwis_logownia/src/main/java/org/pl/serwis_logownia/AuthService.java
@@ -1,7 +1,11 @@
 package org.pl.serwis_logownia;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthService {
+
+    @GetMapping("/login/")
+    private void login(String username, String password) {}
 }
