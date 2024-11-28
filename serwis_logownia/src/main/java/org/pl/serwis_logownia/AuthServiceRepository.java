c51009a768611f24f8113539029f1149d1b44a93
Kacper <kacper.klimas@poczta.fm>
2024-11-28 02:21:34 +0100

    Modified structure

--- serwis_logownia/src/main/java/org/pl/serwis_logownia/AuthServiceRepository.java
+++ serwis_logownia/src/main/java/org/pl/serwis_logownia/AuthServiceRepository.java
@@ -1,4 +1,14 @@
 package org.pl.serwis_logownia;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AuthServiceRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 }
