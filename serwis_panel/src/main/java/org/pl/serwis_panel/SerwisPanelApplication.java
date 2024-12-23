c9657fe7b635fe0f8cc3202da2c940747b509548
Kacper <kacper.klimas@poczta.fm>
2024-12-23 05:35:40 +0100

    Added serwis_panel

---
+++ serwis_panel/src/main/java/org/pl/serwis_panel/SerwisPanelApplication.java
@@ -0,0 +1,13 @@
+package org.pl.serwis_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisPanelApplication.class, args);
+    }
+
+}
---
+++ serwis_panel/src/test/java/org/pl/serwis_panel/SerwisPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pl.serwis_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
