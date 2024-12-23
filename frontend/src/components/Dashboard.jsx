a041d4ddb382812b637dc184b57f2410dec3095c
Kacper <kacper.klimas@poczta.fm>
2024-12-23 19:21:47 +0100

    Added React module

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Dashboard() {
+    const navigate = useNavigate();
+
+    const handleNavigation = (role) => {
+        navigate(`/${role}`);
+    };
+
+    // nav handlers
+    return (
+        <div className="dashboard-main">
+            <div className="dashboard-header">
+                <div className="top-right">
+                    <button onClick={() => navigate("/account")}>
+                        <i className="bi bi-person-circle"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="dashboard-body">
+                <div className="icon-grid">
+                    <button onClick={() => handleNavigation("security")}>
+                        <h1>xd</h1>
+                        <p>Security Pane</p>
+                    </button>
+                    <button onClick={() => handleNavigation("admin")}>Administrator Pane</button>
+                    <button onClick={() => handleNavigation("pilot")}>Pilot Pane <i className="bi bi-airplane"></i>
+                    </button>
+                    <button onClick={() => handleNavigation("staff")}>Airport Staff Pane</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
