e32e7de9f412bda263038bd49a2c367f08b7c912
Kacper <kacper.klimas@poczta.fm>
2024-12-24 03:14:03 +0100

    Working Fullstack version

--- serwis_panel/src/main/java/org/pl/serwis_panel/config/SecurityConfig.java
+++ serwis_panel/src/main/java/org/pl/serwis_panel/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/panel/admin/", "panel/admin/user", "panel/admin/user/*")
+                        .requestMatchers("panel/admin/", "panel/admin/user", "panel/admin/user/*")
                         .permitAll()
                         .anyRequest().authenticated()
                 ).csrf(csrf -> csrf.disable())
--- serwis_panel/src/main/java/org/pl/serwis_panel/config/WebConfig.java
+++ serwis_panel/src/main/java/org/pl/serwis_panel/config/WebConfig.java
@@ -1,6 +1,5 @@
 package org.pl.serwis_panel.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Adjust the origin as needed
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
