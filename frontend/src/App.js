954b7be1aab0e8c55e436d0de36642d3d2ff72df
Simon <szymon.franz@interia.pl>
2024-12-25 16:33:30 +0100

    Added frontend authentication through PrivateRoute.jsx and AuthProvider.jsx, modified App.js, now the users are unable to manually put in /dashboard in the link without authenticating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,19 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import './App.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider } from "./components/AuthProvider";
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/login" />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/dashboard" element={<PrivateRoute element={Dashboard} />} />
+                </Routes>
+            </AuthProvider>
         </Router>
     );
 }
