e9971e86dc333302eb1540849d4efdfadf404f88
Simon <szymon.franz@interia.pl>
2024-12-29 02:14:01 +0100

    added staff pane and navigation to other panes

---
+++ frontend/src/components/AdminPane.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PK from "../img/PK.png";
+
+function AdminPane() {
+    const navigate = useNavigate();
+
+    // nav handlers
+    return (
+        <div className="main-container">
+            <div className="page-header red">
+                <div className="page-header-image">
+                    <img src={PK} alt="PK logo" className="pklogo-header"/>
+                </div>
+                <div className="page-header-button">
+                    <button className="header-button" onClick={() => navigate("/dashboard")}>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AdminPane;
