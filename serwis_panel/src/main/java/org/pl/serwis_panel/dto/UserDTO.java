02b0f1ac0b3d56afc06973b830014f4fffd0f3cb
Kacper Klimas <10kacper.klimas.lo1@gmail.com>
2025-01-02 00:07:40 +0100

    Added some admin functions

--- serwis_panel/src/main/java/org/pl/serwis_panel/dto/UserDTO.java
+++ serwis_panel/src/main/java/org/pl/serwis_panel/dto/UserDTO.java
@@ -1,4 +1,13 @@
 package org.pl.serwis_panel.dto;
 
+import lombok.Data;
+import org.pl.serwis_panel.enums.Role;
+
+@Data
 public class UserDTO {
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private Role role;
 }
